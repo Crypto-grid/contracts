f52d69dd84f61d64adebd7ff553149d07177d032
fbrcode <fabio.bressler@gmail.com>
2022-04-28 10:02:39 -0300

    additional comments and token allocation / grants

--- contracts/tokens/Grid.sol
+++ contracts/tokens/Grid.sol
@@ -11,29 +11,39 @@ contract Grid is ERC20Capped, Ownable {
     uint256 private immutable MAXIMUM_SUPPLY = 750000000*1e18;
     uint256 private immutable INITIAL_SUPPLY = 25000000*1e18;
 
-    // token holders
-    address public marketing;
-    address public treasury;
-    address public liquidityPool;
+    // token holders / spenders
+    address public marketing; // initial 10% allocation to marketing to gather new players to the game (5 year based schedule)
+    address public treasury; // initial supply 10% allocation to treasury multi-sig to fund our operation costs and hire staff
+    address public liquidityPool; // initial supply 30% allocation to liquidity pool on DEX like sushiswap
+    address public developmentSpender; // initial supply 25% approval to developer multi-sig wallet to fund development of the game
+    address public incentivesSpender; // initial supply 25% approval for incentives for players to stake their tokens into sushiswap farm and ensure stability of price
 
     /// @notice GRID contract startup
     /// @dev Name: CryptoGrid,  Symbol: GRID, Decimals: 18 
-    constructor(address _marketing, address _treasury, address _liquidityPool) ERC20("CryptoGrid", "GRID") ERC20Capped(MAXIMUM_SUPPLY) onlyOwner {
+    constructor(
+      address _marketing, 
+      address _treasury, 
+      address _liquidityPool,
+      address _devSpender,
+      address _incentivesSpender
+    ) ERC20("CryptoGrid", "GRID") ERC20Capped(MAXIMUM_SUPPLY) onlyOwner {
         // set initial supply creator/owner
         _mint(msg.sender, INITIAL_SUPPLY);
 
-        // allocate to given parties on contract creation
-
-        // 10% to marketing
+        // allocate or delegate tokens to given parties on contract creation
         marketing = _marketing;
-        _transfer(msg.sender, _marketing, INITIAL_SUPPLY/10);
+        transfer(_marketing, INITIAL_SUPPLY/10);
 
-        // 10% to treasury
         treasury = _treasury;
-        _transfer(msg.sender, _treasury, INITIAL_SUPPLY/10);
+        transfer(_treasury, INITIAL_SUPPLY/10);
 
-        // 30% to liquidity pool
         liquidityPool = _liquidityPool;
-        _transfer(msg.sender, _liquidityPool, INITIAL_SUPPLY*3/10);
+        transfer(_liquidityPool, INITIAL_SUPPLY*3/10);
+
+        developmentSpender = _devSpender;
+        approve(_devSpender, INITIAL_SUPPLY/4);
+
+        incentivesSpender = _incentivesSpender;
+        approve(_incentivesSpender, INITIAL_SUPPLY/4);
     }
 }
