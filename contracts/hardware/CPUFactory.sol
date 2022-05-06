73157bfad192bfe1ed2db05724719756f0742901
provsalt <gooraymond39@gmail.com>
2022-05-07 01:33:42 +0800

    CPU: Add more function to determind price of token

--- contracts/hardware/CPUFactory.sol
+++ contracts/hardware/CPUFactory.sol
@@ -3,15 +3,22 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./cpu/CPU.sol";
 
 contract CPUFactory is Ownable {
-    
-    constructor() public {
+	// This array is used to store all available CPUs that can be sold
+    CPU[] public availableCPUs_;
 
+	address public aggregatorAddress_;
+
+    constructor(address _aggregatorAddress) public {
+		aggregatorAddress_ = _aggregatorAddress;
+		createNewCPU("Intel", "i5", "4990k", 0);
     }
 
     // createNewCPU creates a new NFT based CPU which can be classified as a brand of CPU, series and name of the CPU
-    function createNewCPU(string memory _brand, string memory series, string memory name) public onlyOwner {
-        
+    function createNewCPU(string memory _brand, string memory _series, string memory _name, uint256 _basePrice) public onlyOwner {
+		CPU cpu = new CPU(_brand, _series, _name, _basePrice, aggregatorAddress_);
+		availableCPUs_.push(cpu);
     }
 }
--- contracts/hardware/cpu/CPU.sol
+++ contracts/hardware/cpu/CPU.sol
@@ -6,20 +6,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract CPU is ERC721, Ownable {
-    AggregatorV3Interface internal priceFeed;
-    uint256 public basePrice_;
+	// This pricefeed will be for the ETH/USD so there is some variation in the price
+	AggregatorV3Interface internal priceFeed;
+	uint256 public idCount_ = 0;
+	uint256 public basePrice_;
 
-	constructor(string memory _brand, string memory _series, string memory _name, uint256 _basePrice, address _priceFeed) ERC721(string(abi.encodePacked(_brand, _series, _name)), "gridCPU") {
+	constructor(
+		string memory _brand,
+		string memory _series,
+		string memory _name,
+		uint256 _basePrice,
+		address _priceFeed
+	) ERC721(string(abi.encodePacked(_brand, " ", _series, " ", _name)), "gridCPU") {
 		priceFeed = AggregatorV3Interface(_priceFeed);
 		basePrice_ = _basePrice;
 	}
 
-    function getBasePrice() public view returns (uint256) {
-        return basePrice_;
-    }
+	function getBasePrice() public view returns (uint256) {
+		return basePrice_;
+	}
 
 	// changeBasePrice should only be executed once a governance vote has passed
-    function changeBasePrice(uint256 _basePrice) public onlyOwner {
+	function changeBasePrice(uint256 _basePrice) public onlyOwner {
 		basePrice_ = _basePrice;
+	}
+
+	function mint() public {
+		// TODO: Add minting price based on pricefeed and base price
+
+		// TODO: Implement logic for randomness with chainlink VRF v2 or something
+		_safeMint(msg.sender, idCount_);
+		idCount_++;
+	}
+
+	function tokenURI(uint256 tokenId) public view override returns (string memory) {
+		// TODO: Store tokenURI off chain
 	}
 }
