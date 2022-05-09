fb54e135e6e820fde4f1e1b97abae77077f5735b
Kok Sing <70791269+koksing456@users.noreply.github.com>
2022-05-09 15:50:31 +0800

    Update Grid contract to @openzeppelin/contracts-upgradable

--- deploy/00_deploy_grid_token.ts
+++ deploy/00_deploy_grid_token.ts
@@ -1,18 +1,28 @@
-import { getUnnamedAccounts } from 'hardhat';
+import {getUnnamedAccounts, ethers, upgrades} from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { isBoxedPrimitive } from 'util/types';
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  const randomAccounts = await getUnnamedAccounts()
+  const randomAccounts = await getUnnamedAccounts();
   await deploy('Grid', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     args: [20000000, randomAccounts[0], randomAccounts[1], randomAccounts[2]],
     log: true,
   });
+
+  const grid = await ethers.getContractFactory('Grid');
+  const gridProxy = await upgrades.deployProxy(grid, {
+    initializer: "initialize",
+  });
+
+  await gridProxy.deployed();
+  console.log("gridProxy deployed to: ", gridProxy.address);
+  
 
   /*
     // Getting a previously deployed contract
