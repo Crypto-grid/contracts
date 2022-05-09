1a19ebe2f5879fdbc6a2cc5aa0ebcdbf9f10c2f1
provsalt <gooraymond39@gmail.com>
2022-05-09 15:39:22 +0800

    CPU.sol: Implement Rarities

--- contracts/hardware/cpu/CPU.sol
+++ contracts/hardware/cpu/CPU.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../../interfaces/IBERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "base64-sol/base64.sol";
+import "../../interfaces/IBERC20.sol";
 
 contract CPU is ERC721, Ownable {
 	// This pricefeed will be for the ETH/USD so there is some variation in the price
@@ -13,17 +14,36 @@ contract CPU is ERC721, Ownable {
 	uint256 public basePrice_;
 	address public upgradeToken_;
 
+	enum Rarity {
+		Common,
+		Uncommon,
+		Rare,
+		Epic,
+		Legendary
+	}
+
+	// Rarity of the CPU assigned to the token id of the cpu.
+	// This is determinded at the time of minting of the token.
+	mapping(uint256 => Rarity) public tokenIDRarityMapping;
+	// Rarity to image svg mapping.
+	mapping(Rarity => string) public rarityToImageMapping;
+
 	constructor(
 		string memory _brand,
 		string memory _series,
 		string memory _name,
 		uint256 _basePrice,
 		address _upgradeToken,
-		address _priceFeed
-	) ERC721(string(abi.encodePacked(_brand, " ", _series, " ", _name)), "gridCPU") {
+		address _priceFeed,
+		string memory _imageURI
+	)
+		// string _imageLegendaryURI etc...
+		ERC721(string(abi.encodePacked(_brand, " ", _series, " ", _name)), "gridCPU")
+	{
 		priceFeed = AggregatorV3Interface(_priceFeed);
 		basePrice_ = _basePrice;
 		upgradeToken_ = _upgradeToken;
+		rarityToImageMapping[Rarity.Common] = _imageURI;
 	}
 
 	function getBasePrice() public view returns (uint256) {
@@ -43,6 +63,7 @@ contract CPU is ERC721, Ownable {
 
 		// TODO: Implement logic for randomness with chainlink VRF v2 or something
 		_safeMint(msg.sender, idCount_);
+		tokenIDRarityMapping[idCount_] = Rarity.Common;
 		idCount_++;
 	}
 
@@ -54,7 +75,39 @@ contract CPU is ERC721, Ownable {
 		return ((1000 * 1e18) * basePrice_) / uPrice;
 	}
 
+	function _baseURI() internal pure override returns (string memory) {
+		return "data:application/json;base64,";
+	}
+
 	function tokenURI(uint256 tokenId) public view override returns (string memory) {
-		// TODO: Store tokenURI off chain
+		Rarity _rarity = tokenIDRarityMapping[tokenId];
+		if (_rarity == Rarity.Common) {
+			return
+				string(
+					abi.encodePacked(
+						_baseURI(),
+						Base64.encode(
+							bytes(
+								abi.encodePacked(
+									"{\"name\":",
+									name(), // You can add whatever name here
+									"\", \"description\": \"A CPU based hardware for the game cryptogrid\",",
+									"\"attributes\": [\"rarity\": ",
+									_rarity,
+									"]\"image\":\"",
+									rarityToImageMapping[_rarity],
+									"}"
+								)
+							)
+						)
+					)
+				);
+		}
+	}
+
+	function svgToImageURI(string memory svg) public pure returns (string memory) {
+		string memory baseURL = "data:image/svg+xml;base64,";
+		string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+		return string(abi.encodePacked(baseURL, svgBase64Encoded));
 	}
 }
--- contracts/tokens/Upgrades.sol
+++ contracts/tokens/Upgrades.sol
@@ -4,6 +4,26 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Upgrades is ERC20Burnable{
-    constructor() ERC20("Upgrades", "UPGRADES") {}
+contract Upgrades is ERC20Burnable {
+	constructor() ERC20("Upgrades", "UPGRADES") {}
+
+	address[] public allowedAddresses_;
+
+	// prevent transfer of tokens to prevent users from transfering to themselves
+	// and transfering to other players to progress faster.
+	function _beforeTokenTransfer (
+		address from,
+		address to,
+		uint256 amount
+	) internal virtual override {
+		bool allowed = false;
+		for (uint256 i = 0; i < allowedAddresses_.length; i++) {
+			if (allowedAddresses_[i] == to) {
+				allowed = true;
+				return;
+			}
+		}
+
+		require(allowed, "Transfer to unwhitelisted address is not allowed");
+	}
 }
