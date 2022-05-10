e01c27934b2aa89b4622358578ed50b4188fcf75
provsalt <gooraymond39@gmail.com>
2022-05-10 20:01:45 +0800

    Account.sol: Implement basic account logic

---
+++ contracts/account/Account.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+
+contract Account {
+	struct Acccount {
+		uint256 rebirths;
+		uint256 sacrifices;
+	}
+
+	mapping(address => Acccount) public accounts;
+
+	function signUp(address _address) public {
+		accounts[_address] = Acccount(0, 0);
+	}
+
+	function rebirth(address _address) public {
+		accounts[_address].rebirths++;
+	}
+
+	function sacrifice(address _address) public {
+		accounts[_address].sacrifices++;
+		accounts[_address].rebirths = 0;
+	}
+}
