583c8d4c232a35a31483a537afda2b2918827322
fbrcode <fabio.bressler@gmail.com>
2022-05-13 15:20:22 -0300

    apply prettier

--- test/index.ts
+++ test/index.ts
@@ -1,26 +1,23 @@
-import { expect } from "chai";
-import { ethers, upgrades} from "hardhat";
+import { expect } from 'chai';
+import { ethers, upgrades } from 'hardhat';
 
-describe("Grid", function () {
+describe('Grid', function () {
+	it('Should deploy Grid tokens with 25M minted on deployment', async function () {
+		const signers = await ethers.getSigners();
+		// const treasuryAddress = signers[1].address
 
-  it("Should deploy Grid tokens with 25M minted on deployment", async function () {
+		// const gridFactory = await ethers.getContractFactory("Grid");
+		// const grid = await gridFactory.deploy(treasuryAddress);
+		// const grid = await gridFactory.deploy();
+		// await grid.deployed();
+		const grid = await ethers.getContractFactory('Grid');
+		const gridProxy = await upgrades.deployProxy(grid, {
+			initializer: 'initialize',
+		});
 
-    const signers = await ethers.getSigners();
-    // const treasuryAddress = signers[1].address
-
-    // const gridFactory = await ethers.getContractFactory("Grid");
-    // const grid = await gridFactory.deploy(treasuryAddress);
-    // const grid = await gridFactory.deploy();
-    // await grid.deployed();
-	const grid = await ethers.getContractFactory('Grid');
-    const gridProxy = await upgrades.deployProxy(grid, {
-      initializer: "initialize",
-    });
-
-    // check if initial supply is accurate initial supply
-    const initialSupply = await gridProxy.totalSupply();
-    // console.log('Should mint (25M*10**18) 2.5e+25 Grid tokens ==>', Number(totalSupply));
-    expect(Number(initialSupply)).to.equal(25000000 * 10 ** 18);
-
-  });
+		// check if initial supply is accurate initial supply
+		const initialSupply = await gridProxy.totalSupply();
+		// console.log('Should mint (25M*10**18) 2.5e+25 Grid tokens ==>', Number(totalSupply));
+		expect(Number(initialSupply)).to.equal(25000000 * 10 ** 18);
+	});
 });
