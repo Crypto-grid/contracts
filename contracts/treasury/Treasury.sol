91a728443b86a8d38c79b3593b2f21df076cbfd8
fbrcode <fabio.bressler@gmail.com>
2022-05-14 09:01:13 -0300

    prettier

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -21,10 +21,10 @@ contract Treasury {
 		address _admin2,
 		address _admin3
 	) internal {
-    address[3] memory _admin;
-    _admin[0] = _admin1;
-    _admin[1] = _admin2;
-    _admin[2] = _admin3;
+		address[3] memory _admin;
+		_admin[0] = _admin1;
+		_admin[1] = _admin2;
+		_admin[2] = _admin3;
 		treasuryAdmin.setAdminstrators(_admin);
 	}
 
