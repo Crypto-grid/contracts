0424d5bfb078957a9ef9a4223ce5346eb86c789b
fbrcode <fabio.bressler@gmail.com>
2022-05-22 18:14:41 -0300

    recaftor grid feployment

--- deploy/00_deploy_grid_token.ts
+++ deploy/00_deploy_grid_token.ts
@@ -3,6 +3,17 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import fs from 'fs'; // to save the contract address locally
 
+// const exportContract = async (fileName: string, address: string) => {
+//   try {
+//     fs.writeFileSync(
+//       fileName, 
+//       `export const gridProxyContractAddress=${address}`
+//     );
+//   } catch (err) {
+//     console.log(err);
+//   }  
+// }
+
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 	const { getNamedAccounts, deployments } = hre;
   const chainId: number | undefined = network.config.chainId
@@ -11,26 +22,39 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 	const randomAccounts = await getUnnamedAccounts();
 	
   // deploy grid contract  
-	const Grid = await ethers.getContractFactory('Grid');
-	const gridInstance = await upgrades.deployProxy(
-    Grid, [ 
-      randomAccounts[0], 
-      randomAccounts[1] 
-    ]
-  );
-	await gridInstance.deployed();
-	log(`grid deployed to: ${gridInstance.address}`);
+  const deployed = await deploy('Grid', {
+		from: deployer,
+		log: true,
+	});
+
+  // call initialize with...
+  // args: [randomAccounts[0], randomAccounts[1]]
+
+  // const deployed = await deploy('Grid', {
+	// 	from: deployer,
+	// 	args: [randomAccounts[0], randomAccounts[1]],
+	// 	log: true,
+	// });
+
+  // const Grid = await ethers.getContractFactory('Grid');
+	// const gridInstance = await upgrades.deployProxy(
+  //   Grid, [ 
+  //     randomAccounts[0], 
+  //     randomAccounts[1] 
+  //   ]
+  // );
+	// await gridInstance.deployed();
+	// log(`GRID deployed to: ${JSON.stringify(deployed,null,2)}`);
+	log(`GRID deployed to: ${deployed.address}`)
 
   // save contract address
-  try {
-    fs.writeFileSync(
-      //'../generated/contract-types/gridAddress.ts', 
-      'gridAddress.ts', 
-      `export const gridProxyContractAddress=${gridInstance.address}`
-    );
-  } catch (err) {
-    console.error(err);
-  }  
+  // await exportContract('gridAddress.ts', gridInstance.address);
+  // fs.writeFile("gridAddress.ts", "Hey there!", function(err) {
+  //   if(err) {
+  //       return console.log(err);
+  //   }
+  //   // console.log("The file was saved!");
+  // }); 
 
 };
 export default func;
